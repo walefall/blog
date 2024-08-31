89c768af8322e4ad97086062392a11f437a18813
walefall <1255749769@qq.com>
2024-08-31 21:56:03 +0800

    Site updated: 2024-08-31 21:56:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/hello-world/"];function toRandomPost(){
+var posts=["2024/08/31/hello-world - 副本/","2024/08/31/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
